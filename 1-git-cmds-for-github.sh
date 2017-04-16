044aac2b74e95da7ccf826da7fbec54f4efafca4
Siyam-Khan <siyam.sajeeb79@gmail.com>
2017-04-16 22:35:04 +0600

    business release : v2--10-4-17

---
+++ 1-git-cmds-for-github.sh
@@ -0,0 +1,8 @@
+echo "# JS Calculator" >> README.md
+git init
+git add .
+git commit -m "business release : v2--10-4-17"
+git remote add origin https://github.com/Siyam-Khan/JavaScript
+git remote -v
+git push origin master
+git push --all -f https://github.com/Siyam-Khan/JavaScript
